d6afa8a361ef4d50935dd054863fc7d01e3d8105
madness-613 <austinfaulknerjanes2008@gmail.com>
2024-03-05 17:10:51 -0500

    commands split

--- src/main/java/frc/robot/commands/ClimbCommands/SetLeftClimbHeight.java
+++ src/main/java/frc/robot/commands/ClimbCommands/SetLeftClimbHeight.java
@@ -2,18 +2,18 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Climb;
+package frc.robot.commands.ClimbCommands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Climb.RightClimb;
+import frc.robot.subsystems.Climb.LeftClimb;
 
-public class SetRightClimbHeight extends Command {
-  /** Creates a new SetRightClimbHight. */
-  public final RightClimb climb;
-  public final PIDController pid = new PIDController(0.05, 0, 0);
-  public final double height;
-  public SetRightClimbHeight(RightClimb climb, double height) {
+public class SetLeftClimbHeight extends Command {
+  /** Creates a new SetClimbHight. */
+  private final PIDController pid = new PIDController(0.05, 0, 0);
+  private LeftClimb climb;
+  private double height;
+  public SetLeftClimbHeight(LeftClimb climb, Double height) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.climb = climb;
     this.height = height;
@@ -29,12 +29,14 @@ public class SetRightClimbHeight extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    climb.set(pid.calculate(climb.getPosition(), height));
+    climb.set(pid.calculate(climb.getPosition() ,height));
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climb.stop();
+  }
 
   // Returns true when the command should end.
   @Override
