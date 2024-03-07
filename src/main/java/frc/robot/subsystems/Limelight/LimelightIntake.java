54dc7c1adefebf86eb97b1b465a29c4732221562
madness-613 <austinfaulknerjanes2008@gmail.com>
2024-03-07 14:57:07 -0500

    split limelight

--- src/main/java/frc/robot/subsystems/Limelight/LimelightIntake.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightIntake.java
@@ -2,16 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.Limelight;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LimelightTag extends SubsystemBase {
-  private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-
-  public LimelightTag() {}
+public class LimelightIntake extends SubsystemBase {
+  /** Creates a new LimelightIntake. */
+  private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-intake");
+  public LimelightIntake() {}
 
   @Override
   public void periodic() {
